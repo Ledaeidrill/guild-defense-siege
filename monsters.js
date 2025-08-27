f0b5bd2fd2f40e4dd097c150cf0c0a4b8946d333
Ledaeidrill <95937046+Ledaeidrill@users.noreply.github.com>
2025-08-27 17:13:35 +0200

    Create monsters.js

---
+++ monsters.js
@@ -0,0 +1,13 @@
+// Chaque entrée : { id, name, icon }
+// icon peut être une URL (Swarfarm, SWGT...) ou un fichier local dans /icons
+window.MONSTERS = [
+  {id: 1, name: 'Molly',     icon: 'https://i.imgur.com/7Q0s7sB.png'},
+  {id: 2, name: 'Kinky',     icon: 'https://i.imgur.com/9g3j9xX.png'},
+  {id: 3, name: 'Vigor',     icon: 'https://i.imgur.com/1V2lYkW.png'},
+  {id: 4, name: 'Clara',     icon: 'https://i.imgur.com/5nQkH8r.png'},
+  {id: 5, name: 'Savannah',  icon: 'https://i.imgur.com/0x2fV4g.png'},
+  {id: 6, name: 'Dominic',   icon: 'https://i.imgur.com/0lCk0tS.png'},
+  {id: 7, name: 'Iris',      icon: 'https://i.imgur.com/Qq6m3sQ.png'},
+  {id: 8, name: 'Theo',      icon: 'https://i.imgur.com/8D0o9rN.png'},
+  // ... ajoute la suite ici
+];
